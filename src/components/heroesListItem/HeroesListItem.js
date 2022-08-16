20cdfbfaf816755b24fabe4e1e62e3d658a1ed06
Rostyslav <bovonenko@icloud.com>
2022-08-16 16:28:20 +0200

    changed img

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -23,7 +23,7 @@ const HeroesListItem = ({name, description, element, onDelete}) => {
         <li 
            
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
+            <img src="https://www.kindpng.com/picc/m/52-526072_unknown-character-hd-png-download.png" 
                  className="img-fluid w-25 d-inline" 
                  alt="unknown hero" 
                  style={{'objectFit': 'cover'}}/>
